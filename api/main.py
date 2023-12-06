93fb0754a328217682f116ca5fdc8374b1835069
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-12-06 23:14:45 +0100

    chg: [api] Added simple query to the kvrocks database with the base64 encoded payload.

--- api/main.py
+++ api/main.py
@@ -1,10 +1,16 @@
 from typing import Union
 
+import redis
 from fastapi import FastAPI
 
 app = FastAPI()
 
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-@app.get("/items/{payload}")
-def read_item(payload: str, q: Union[str, None] = None):
-    return {"item_id": payload, "q": q}
+@app.get("/items/{base64_payload}")
+def read_item(base64_payload: str, q: Union[str, None] = None):
+    if q:
+        item = r.ft().search(q)
+    else:
+        item = r.get(base64_payload)
+    return {"item": item, "q": q}
