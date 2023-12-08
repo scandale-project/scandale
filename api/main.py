e7aa0227143e63590263088d6ca72e357382158a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-12-08 22:09:51 +0100

    chg: [api] testing with PostgreSQL (psycopg2) database.

--- api/main.py
+++ api/main.py
@@ -1,12 +1,29 @@
 from typing import Union, Dict
 
 import redis
+import psycopg2
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 r = redis.Redis(host='localhost', port=6666, db=0)
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    dbname="pumpkin",
+    user="cedric",
+    password="your_password",
+    host="127.0.0.1",
+    port=""
+)
+
+# Create a cursor object to interact with the database
+cursor = conn.cursor()
+
+# Define a table in PostgreSQL to store JSON data
+table_name = "scan_data"
+cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id SERIAL PRIMARY KEY, data JSONB)")
 
 
 class Payload(BaseModel):
@@ -39,11 +56,15 @@ async def read_item(base64_payload: str, q: Union[str, None] = None): # -> Item:
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item):
     """Insert a new item."""
-    res = r.set(item.payload.row, item.model_dump_json())
-    # res = r.sadd("items", item.model_dump_json())
+    # res = r.set(item.payload.row, item.model_dump_json())
+    #   res = r.sadd("items", item.model_dump_json())
     # print(item.payload.row)
     # print(item.model_dump_json())
-    print(res)
+    json_string = json.dumps(item)
+    # Insert the JSON data into the PostgreSQL table
+    cursor.execute(f"INSERT INTO {table_name} (data) VALUES (%s)", (json_string,))
+    # Commit the changes
+    conn.commit()
     return item
 
 
