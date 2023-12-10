50208d46d0914671488a7334d063f23d2f7689c8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-12-10 23:07:04 +0100

    chg: [API] Removed code related to redis since the API is now using psycopg2.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
-from typing import Union, Dict
+from typing import Union
 
-import redis
 import psycopg2
 import json
 from fastapi import FastAPI
@@ -8,22 +7,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-r = redis.Redis(host='localhost', port=6666, db=0)
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
     dbname="pumpkin",
     user="cedric",
-    password="your_password",
+    password="password",
     host="127.0.0.1",
-    port=""
+    port=5432
 )
 
 # Create a cursor object to interact with the database
 cursor = conn.cursor()
 
 # Define a table in PostgreSQL to store JSON data
-table_name = "scan_data"
-cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id SERIAL PRIMARY KEY, data JSONB)")
+TABLE_NAME = "scan_data"
+cursor.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id SERIAL PRIMARY KEY, data JSONB)")
 
 
 class Payload(BaseModel):
@@ -43,26 +41,15 @@ class Item(BaseModel):
 
 @app.get("/items/{base64_payload}")
 async def read_item(base64_payload: str, q: Union[str, None] = None): # -> Item:
-    if q:
-        for key in r.scan_iter(f"{q}:*"):
-            print(key)
-            item = key
-    else:
-        item = r.get(base64_payload)
-    print(item)
-    return item
+    return {}
 
 
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item):
     """Insert a new item."""
-    # res = r.set(item.payload.row, item.model_dump_json())
-    #   res = r.sadd("items", item.model_dump_json())
-    # print(item.payload.row)
-    # print(item.model_dump_json())
     json_string = json.dumps(item)
     # Insert the JSON data into the PostgreSQL table
-    cursor.execute(f"INSERT INTO {table_name} (data) VALUES (%s)", (json_string,))
+    cursor.execute(f"INSERT INTO {TABLE_NAME} (data) VALUES (%s)", (json_string,))
     # Commit the changes
     conn.commit()
     return item
@@ -71,4 +58,6 @@ async def create_item(item: Item):
 @app.get("/stats/")
 async def stats():
     """Provides stats about the database."""
-    return {"dbsize": r.scard("items")}
+    res = cursor.execute(f"select count(*) from {TABLE_NAME};")
+    results = cursor.fetchone()
+    return {"dbsize": results[0]}
