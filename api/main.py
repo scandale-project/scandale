345f3db8793cc5772bdf01087067e329dac58c4a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-12-11 10:54:38 +0100

    new: [core] Added crud, models and schemas modules for the API.

--- api/main.py
+++ api/main.py
@@ -1,63 +1,39 @@
 from typing import Union
 
-import psycopg2
 import json
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(
-    dbname="pumpkin",
-    user="cedric",
-    password="password",
-    host="127.0.0.1",
-    port=5432
-)
-
-# Create a cursor object to interact with the database
-cursor = conn.cursor()
-
-# Define a table in PostgreSQL to store JSON data
-TABLE_NAME = "scan_data"
-cursor.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (id SERIAL PRIMARY KEY, data JSONB)")
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-class Payload(BaseModel):
-    row: str
-
-class Meta(BaseModel):
-    uuid: str
-    ts: str
-    type: str
-
-class Item(BaseModel):
-    version: str
-    format: str
-    meta: Meta
-    payload: Payload
+@app.get("/items/", response_model=list[schemas.ItemBase])
+async def read_item(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
 
 
-@app.get("/items/{base64_payload}")
-async def read_item(base64_payload: str, q: Union[str, None] = None): # -> Item:
-    return {}
-
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
+@app.post("/items/", response_model=schemas.ItemBase)
+async def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     """Insert a new item."""
-    json_string = json.dumps(item)
-    # Insert the JSON data into the PostgreSQL table
-    cursor.execute(f"INSERT INTO {TABLE_NAME} (data) VALUES (%s)", (json_string,))
-    # Commit the changes
-    conn.commit()
-    return item
+    # json_string = json.dumps(item)
+    return crud.create_item(db=db, item=item)
 
 
 @app.get("/stats/")
 async def stats():
     """Provides stats about the database."""
-    res = cursor.execute(f"select count(*) from {TABLE_NAME};")
-    results = cursor.fetchone()
-    return {"dbsize": results[0]}
+    return {"dbsize": 0}
